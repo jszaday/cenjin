12b783703a9bc02451b9f368c915f1600b9fa9c9
Justin Szaday <jjszaday@gmail.com>
2023-05-25 18:56:50 -0700

    add comment

--- src/main/scala/com/justinszaday/cenjin/Main.scala
+++ src/main/scala/com/justinszaday/cenjin/Main.scala
@@ -7,6 +7,6 @@ object Main {
     val value = Value("int", "x", Some("42"))
     val visitor = new CppCodeGenerator
     implicit val ctx = new CppCodeGenerator.Context
-    println(visitor.visit(value))
+    println(visitor.visit(value))  // => int x = 42
   }
 }
